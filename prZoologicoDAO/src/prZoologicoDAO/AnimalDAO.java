ff66f3bcb22e902bb1b1a778fbcfabb4ab67f1e3
Alejandro Asencio <alexcelia910@gmail.com>
2022-10-21 13:33:14 +0200

    findAnimalById

--- prZoologicoDAO/src/prZoologicoDAO/AnimalDAO.java
+++ prZoologicoDAO/src/prZoologicoDAO/AnimalDAO.java
@@ -2,12 +2,76 @@ package prZoologicoDAO;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public abstract class AnimalDAO {
 
 	private static Connection connection;
 	
+	//Buscar animal por id
+	public static Animal findById(int id) {
+		connection = openConnection();
+		
+		String query = "select * from animales where id = ?";
+		Animal animal = null;
+		try {
+			PreparedStatement ps = connection.prepareStatement(query);
+			ps.setInt(1, id);
+			ResultSet rs = ps.executeQuery();
+			
+			while (rs.next()) {
+				animal = new Animal(
+						rs.getInt("id"),
+						rs.getString("nombre"),
+						rs.getString("habitad"),
+						rs.getDouble("peso_aproximado")
+						);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		closeConnection();
+		
+		return animal;
+	}
+	
+	// Borrar animal por nombre
+	public static void deleteAnimalByNombre(String nombre) { 
+		connection = openConnection(); 
+		
+		String query = "DELETE FROM animales WHERE nombre = ?"; 
+		
+		try { 
+			PreparedStatement ps = connection.prepareStatement(query); 
+			ps.setString(1, nombre); 
+			ps.executeUpdate();
+		} catch (SQLException e) { 
+			// TODO Auto-generated catch block 
+			e.printStackTrace(); 
+		} 
+		closeConnection(); 
+	}
+		
+	//Borrar todos los animales
+	public static void deleteAnimal() {
+		connection = openConnection();
+		
+		String query = "delete from animales";
+		
+		try {
+			Statement statement = connection.createStatement();
+			statement.executeUpdate(query);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		closeConnection();
+	}
+	
+	//	Insertar un animal
 	public static void insertAnimal(Animal animal) {
 		connection = openConnection();
 		
--- prZoologicoDAO/src/prZoologicoDAO/Main.java
+++ prZoologicoDAO/src/prZoologicoDAO/Main.java
@@ -4,9 +4,10 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Animal a = new Animal ("Ardilla", "Bosque", 0.1);
-		AnimalDAO.insertAnimal(a);
-		
+		//Animal a = new Animal ("Ardilla", "Bosque", 0.1);
+		//AnimalDAO.insertAnimal(a);
+		//AnimalDAO.deleteAnimal();
+		AnimalDAO.deleteAnimalByNombre("Ardilla");
 	}
 
 }
